30f004c70a4261e33ae4df11bd1aebd91da2433a
Tom Bouillut <tom@p113.io>
2023-11-25 11:40:09 -0300

    refactor to sync_to_repo.sh  and sync_to_system.sh  and messages.sh  work in 3 languages  install missing packages  for debian, ubuntu and arch new file:   .gitignore modified:   messages.sh modified:   sync_to_repo.sh deleted:    .gsd-keyboard.settings-ported deleted:    QtProject.conf deleted:    Trolltech.conf deleted:    connections.db deleted:    dleyna-renderer-service.conf deleted:    mimeapps.list deleted:    pavucontrol.ini deleted:    polybar/config deleted:    polybar/launch.sh deleted:    polybar/scripts/check-arch-updates.sh deleted:    polybar/scripts/check-aur-updates.sh deleted:    polybar/scripts/get_spotify_status.sh deleted:    polybar/scripts/pavolume.sh deleted:    polybar/scripts/pub-ip.sh deleted:    polybar/scripts/scroll_spotify_status.sh deleted:    polybar/scripts/spotify1.sh deleted:    polybar/scripts/tempcores.sh deleted:    polybar/scripts/weather.py deleted:    rygel.conf deleted:    sway-xdg-terminals.list deleted:    user-dirs.dirs deleted:    user-dirs.locale deleted:    xdg-terminals.list deleted:    yad.conf

--- sync_to_repo.sh
+++ sync_to_repo.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-# Script shell pour synchroniser les fichiers de configuration
-# depuis le dossier ~/.config de l'utilisateur vers un dépôt git
-# Ce script permet de conserver l'historique des modifications
-# et de synchroniser facilement la configuration entre plusieurs machines
-# et utilisateurs
-# Usage : ./sync_to_repo.sh
+# Script shell to synchronize configuration files
+# from the user's ~/.config folder to a git repository
+# This script allows to keep track of modifications
+# and easily synchronize the configuration across multiple machines and users
+# Usage: ./sync_to_repo.sh
 
-# Dossiers de configuration source et cible
+# Source and target configuration folders
 src="$HOME/.config"
 local="$HOME/.local"
 scripts="$HOME/git_config/"
 dest="$HOME/git_config/.config"
 dest_local="$HOME/git_config/.local"
 
-# Identifie la distribution linux et installe les paquets appropriés
-if grep -qEi "(debian|buntu)" /etc/*release; then
+# Identify the Linux distribution and install the appropriate packages
+if grep -qEi "(debian|ubuntu)" /etc/*release; then
   os="Debian/Ubuntu"
 elif [ -f /etc/arch-release ]; then
   os="Arch"
@@ -23,10 +22,10 @@ else
   os="Unknown"
 fi
 
-# Charger les messages depuis le fichier messages.sh
+# Load messages from the messages.sh file
 source "$scripts/messages.sh"
 
-# Détecter la langue de l'utilisateur
+# Detect the user's language
 if [ -z "${LANG}" ]; then
   echo "The system language was not detected."
   echo "Please enter the locale code (e.g., en_US.UTF-8, fr_FR.UTF-8):"
@@ -34,31 +33,25 @@ if [ -z "${LANG}" ]; then
   export LANG="$user_locale"
   user_lang=${user_locale:0:2}
 
-  # Mettre à jour les fichiers de configuration pour Sway, Hyprland, ou Wayland
-  # Remplacer les chemins et les commandes par ceux appropriés pour votre système
+  # Update configuration files for Sway, Hyprland, or Wayland
+  # Replace paths and commands with the appropriate ones for your system
   if command -v sway &>/dev/null; then
-    # Pour Sway, mettez à jour le fichier de configuration
-    echo "export LANG=$user_locale" >>~/.config/sway/config
-    # Vous devrez peut-être redémarrer Sway pour que les changements prennent effet
+    # For Sway, update the configuration file
+    echo "export LANG=$user_locale" >>"$src/sway/config"
+    # You may need to restart Sway for the changes to take effect
   fi
   if command -v hyprland &>/dev/null; then
-    # Pour Hyprland, mettez à jour le fichier de configuration
-    echo "export LANG=$user_locale" >>~/.config/hyprland/hyprland.conf
-    # Vous devrez peut-être redémarrer Hyprland pour que les changements prennent effet
+    # For Hyprland, update the configuration file
+    echo "export LANG=$user_locale" >>"$src/hyprland/hyprland.conf"
+    # You may need to restart Hyprland for the changes to take effect
   fi
-  # Ajoutez ici des commandes similaires pour Wayland ou d'autres gestionnaires de fenêtres
+  # Add similar commands for Wayland or other window managers here
 else
   user_lang=${LANG:0:2}
 fi
-# vérifie si rsync et git sont installés,
-# si non, crée une array pour lister les packets à installer
-# si l'array contient au moins un packet,
-# fait un seul prompt pour confirmer l'installation des packets manquants
-# en fonction de l'os (debian, ubuntu , archlinux) et lancer l'installation
-# sinon, affiche un message d'erreur.
-# ajoute les messages d'erreur dans messages.sh
 
-# Vérifier si rsync et git sont installés
+# Check if rsync and git are installed, install missing packages if necessary
+missing_packages=()
 if ! command -v rsync &>/dev/null; then
   missing_packages+=("rsync")
 fi
@@ -66,45 +59,36 @@ if ! command -v git &>/dev/null; then
   missing_packages+=("git")
 fi
 
-# Afficher les messages d'erreur et quitter le script
+# Install missing packages if prompted
 if [ ${#missing_packages[@]} -gt 0 ]; then
   echo "$(get_message "missing_packages")"
-  exit 1
-fi
-
-# Prompt pour confirmer l'installation des packets manquants
-if [ ${#missing_packages[@]} -gt 0 ]; then
   read -p "$(get_message "install_missing_packages")" -n 1 -r
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     if [ "$os" == "Debian/Ubuntu" ]; then
       sudo apt install -y "${missing_packages[@]}"
     elif [ "$os" == "Arch" ]; then
-      sudo pacman -S "${missing_packages[@]}"
+      yay -S "${missing_packages[@]}"
     fi
   fi
 fi
 
-# Récup/MPL le à jour equitable de la derniere mise à jour git
+# Update the last git update timestamp
 cd "$dest" && last_git_update=$(git log -1 --format="%ct")
 cd "$dest" || exit
+dirs=("nwg-displays" "nwg-dock" "nwg-look" "nwg-panel" "gtklock" "my_bar" "nwg-bar" "nwg-dock-hyprland" "swaync" "nwg-drawer" "sway" "paru" "volumeicon")
 
-# Liste des répertoires à synchroniser
-declare -a dirs=("HybridBar" "nwg-displays" "nwg-dock" "nwg-look" "nwg-panel" "gtklock" "my_bar" "nwg-bar" "nwg-dock-hyprland" "swaync" "nwg-drawer" "sway" "paru" "volumeicon")
-
-# verifie si tous les repertoires existent bien dans $dest
-# sinon propose d'installer les packages manquants
-# fait un seul prompt pour confirmer l'installation des packets manquants
-# en fonction de l'os (debian, ubuntu , archlinux) et lancer l'installation
-# chaque dir manquant est un packet a installer
+# Install missing packages for directories that don't exist
+created_dirs=()
 for dir in "${dirs[@]}"; do
   if [ ! -d "$dest/$dir" ]; then
     created_dirs+=("$dir")
   fi
 done
-# Affiche un seul prompt pour confirmer l'installation des packets manquants
-# en fonction de l'os (debian, ubuntu , archlinux) et lancer l'installation
+
+# Install missing packages if prompted
 if [ ${#created_dirs[@]} -gt 0 ]; then
+  echo "$(get_message "install_missing_packages")"
   read -p "$(get_message "install_missing_packages")" -n 1 -r
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -116,33 +100,17 @@ if [ ${#created_dirs[@]} -gt 0 ]; then
   fi
 fi
 
-# Fait la syncro rsync de config $src vers $dest
-# puis de $local vers $dest_local
-# Synchroniser les répertoires connus
-# Vérifier si des fichiers ont été modifiés ou ajoutés depuis la dernière mise à jour git
-# Trouver et proposer de copier les nouveaux répertoires
-# Synchroniser les répertoires manquants
-
-# Synchroniser les répertoires connus dans $dest et $dest_local
+# Sync directories in $dest and $dest_local
 for dir in "${dirs[@]}"; do
-  if [ ! -d "$dest/$dir" ]; then
-    mkdir -p "$dest/$dir"
-  fi
-  if [ ! -d "$dest_local/$dir" ]; then
-    mkdir -p "$dest_local/$dir"
-  fi
+  mkdir -p "$dest/$dir"
+  mkdir -p "$dest_local/$dir"
   changed_files=$(find "$src/$dir" -type f -newermt "@$last_git_update")
   if [ -n "$changed_files" ]; then
-    echo "Des modifications：
-    cp -r "$src/$dir/"* "$dest/$dir/"
-    cp -r "$local/$dir/"* "$dest_local/$dir/"
+    echo "Modifications found in $dir"
+    rsync -a "$src/$dir/" "$dest/$dir/"
+    rsync -a "$local/$dir/" "$dest_local/$dir/"
   fi
 done
 
-# Affiche le message de fin de syncro
-
-echo "$(get_message "sync_completed")"
-
-# Affiche le message de fin de syncro
-
+# Display sync completion message
 echo "$(get_message "sync_completed")"
