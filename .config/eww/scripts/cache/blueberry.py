10f49a9ad2cd09a8dca157d6023a7145af2ada77
Tom Bouillut <tom@p113.io>
2023-11-26 05:33:21 -0300

    rebase step 1

--- .config/eww/scripts/cache/blueberry.py
+++ .config/eww/scripts/cache/blueberry.py
@@ -1,1 +1,5 @@
+<<<<<<< HEAD:.config/eww/scripts/cache/blueberry.py
 /usr/share/icons/gruvbox-dark-icons-gtk/16x16/mimetypes/application-x-executable.svg
+=======
+
+>>>>>>> b9840e1c11 (update config hypr and eww):eww/scripts/cache/blueberry.py
