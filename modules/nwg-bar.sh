7b443066ee3c87ee68881695b91795e98e37a34a
Tom Bouillut <tom@p113.io>
2023-11-28 05:38:44 -0300

    added nwg-bar, nwg-displays, nwg-drawer

--- modules/nwg-bar.sh
+++ modules/nwg-bar.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 sudo apt install -y golang-go libgtk-3-dev libgtk-layer-shell0
-git clone https://github.com/nwg-piotr/nwg-dock.git
-cd nwg-dock
+git clone https://github.com/nwg-piotr/nwg-bar.git
+cd nwg-bar
 make get
 make build
 sudo make install
 cd ../
-rm -rf nwg-dock
+rm -rf nwg-bar
 sudo apt -y remove --autoremove golang-go
--- modules/nwg-drawer.sh
+++ modules/nwg-drawer.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 sudo apt install -y golang-go libgtk-3-dev libgtk-layer-shell0
-git clone https://github.com/nwg-piotr/nwg-dock.git
-cd nwg-dock
+git clone https://github.com/nwg-piotr/nwg-drawer.git
+cd nwg-drawer || exit
 make get
 make build
 sudo make install
 cd ../
-rm -rf nwg-dock
+rm -rf nwg-drawer
 sudo apt -y remove --autoremove golang-go
