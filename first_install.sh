767cf36b1e9dab2e07960eef1bdad3a183737b75
Tom Bouillut <tom@p113.io>
2023-11-28 23:34:55 -0300

    upfdate progress bar

--- first_install.sh
+++ first_install.sh
@@ -6,7 +6,7 @@ export local_dir="$HOME/.local"
 # shellcheck disable=SC2155
 export scripts="$(dirname "$0")"
 export source_dir="$scripts/.config"
-export modules_source_dir="$scripts/modules"
+export modules_dir="$scripts/modules"
 
 # Set user locale
 export user_locale="en_US.UTF-8"
@@ -33,6 +33,51 @@ mkdir -p "$local_dir/bin"
 # Check that required packages are installed
 required_packages=("make" "rsync" "git" "shellcheck")
 missing_packages=()
+#!/bin/bash
+
+# Fonction pour afficher la barre de progression
+progress_bar() {
+  local duration=$1
+  # shellcheck disable=SC2155
+  local start_time=$(date +%s)
+  local end_time=$((start_time + duration))
+  local current_time=$start_time
+
+  # shellcheck disable=SC2086
+  while [ $current_time -lt $end_time ]; do
+    local current=$((current_time - start_time))
+    local total=$((end_time - start_time))
+    local progress=$((current * 100 / total))
+    local filled=$((progress / 2)) # Diviser par 2 pour ajuster à la largeur de 50
+    local unfilled=$((50 - filled))
+    echo -ne "\r\033[32m["
+    for i in $(seq 1 $filled); do echo -n '#'; done
+    # shellcheck disable=SC2034
+    for i in $(seq 1 $unfilled); do echo -n '-'; done
+    echo -ne "\033[0m] $progress%"
+    current_time=$(date +%s)
+    sleep 1
+  done
+  echo -e "\n"
+}
+
+# Mise à jour de la liste des paquets
+echo "Mise à jour des paquets..."
+progress_bar 5
+sudo apt-get update -q
+
+# Installation du package nécessaire
+echo "Installation du package..."
+progress_bar 10
+sudo apt-get install -y [nom_du_package]
+
+# Nettoyage après installation
+echo "Nettoyage..."
+progress_bar 5
+sudo apt-get autoremove -y
+sudo apt-get clean
+
+echo "Installation et nettoyage terminés."
 
 for package in "${required_packages[@]}"; do
   if ! dpkg -s "$package" &>/dev/null; then
@@ -67,8 +112,8 @@ for package in "${required_packages[@]}"; do
   if ! dpkg -s "$package" &>/dev/null; then
     missing_packages+=("$package")
   fi
-  if [ -d "$modules_source_dir/$package.sh" ]; then
-    cd "$modules_source_dir/$package" || exit
+  if [ -d "$modules_dir/$package.sh" ]; then
+    cd "$modules_dir/$package" || exit
     # shellcheck disable=SC2086
     ./$package.sh
     cd ../
