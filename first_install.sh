b6ed30f1020107b32b654288285ec16b7d518d8a
TomBombadilom <tom@croll.fr>
2023-11-29 20:35:19 -0300

    fix Typo

--- first_install.sh
+++ first_install.sh
@@ -6,7 +6,7 @@ export local_dir="$HOME/.local"
 # shellcheck disable=SC2155
 export scripts="$(dirname "$0")"
 export source_dir="$scripts/.config"
- modules_dir="$scripts/modules"
+export modules_dir="$scripts/modules"
 
 # Set user locale
 export user_locale="en_US.UTF-8"
@@ -87,27 +87,94 @@ packages=(
   "wofi"
   "wtype"
   "wunderlist"
-  "xwallpaper"
-  "xwayland"
-  "zathura"
-  "zathura-pdf-mupdf"
-  "yad"
-)
+ 
+#!/usr/bin/env bash
 
-# Update package list
-echo "Updating package list..."
-sudo apt update -q
+# Configuration source and target directories
+export config_dir="$HOME/.config"
+export local_dir="$HOME/.local"
+# shellcheck disable=SC2155
+export scripts="$(dirname "$0")"
+export source_dir="$scripts/.config"
+export modules_dir="$scripts/modules"
 
-# Install each package individually
-for package in "${packages[@]}"; do
-  echo "Installing $package..."
-  sudo apt install -y $package 2
-  >&1 | tee "log_$package.txt" || echo "Failed to install $package, skipping..."
-done
+# Set user locale
+export user_locale="en_US.UTF-8"
+export LANG="$user_locale"
+user_lang=${user_locale:0:2}
+export user_lang="$user_lang"
 
-# Cleanup after installation
-echo "Cleaning up..."
-sudo apt autoremove -y
-sudo apt clean
+# Update configuration files for Sway, Hyprland, or Wayland
+if command -v sway &>/dev/null; then
+  echo "export LANG=$user_locale" >>"$config_dir/sway/config"
+fi
 
-echo "Installation and cleanup completed."
+if command -v hyprland &>/dev/null; then
+  echo "export LANG=$user_locale" >>"$config_dir/hyprland/hyprland.conf"
+fi
+
+# Load messages from messages.sh file
+# shellcheck disable=SC1091
+source ./messages.sh
+
+# Create destination directory if none exists
+mkdir -p "$local_dir/bin"
+# shellcheck disable=SC2034
+packages=(
+  "anytype"
+  "betterlockscreen"
+  "azote"
+  "bc"
+  "blueberry"
+  "bluez"
+  "boost"
+  "breeze"
+  "breeze-gtk"
+  "cava"
+  "copyq"
+  "dmenu"
+  "dunst"
+  "fish"
+  "flameshot"
+  "foot"
+  "grim"
+  "hyprpicker"
+  "hyprshot"
+  "jq"
+  "libinput"
+  "libwayland-server0"
+  "libinput-gestures"
+  "light"
+  "mpd"
+  "mpv"
+  "neofetch"
+  "neovim"
+  "nmtui"
+  "noto-fonts"
+  "noto-fonts-cjk"
+  "noto-fonts-emoji"
+  "noto-fonts-extra"
+  "pavucontrol"
+  "picom"
+  "playerctl"
+  "polybar"
+  "rofi"
+  "sway"
+  "swaybg"
+  "swayidle"
+  "swaylock"
+  "swaync"
+  "thunar"
+  "tint2"
+  "ttf-nerd-fonts-symbols"
+  "unzip"
+  "wayland"
+  "wayland-protocols"
+  "wl-clipboard"
+  "wlsunset"
+  "waybar"
+  "wayfire"
+  "wofi"
+  "wtype"
+  "wunderlist"
+ 
