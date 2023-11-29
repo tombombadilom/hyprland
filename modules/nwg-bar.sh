8b69412456c3edee71ea08433979912f1cdb6c33
Tom Bouillut <tom@p113.io>
2023-11-29 05:27:04 -0300

    update modules

--- modules/nwg-bar.sh
+++ modules/nwg-bar.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo apt install -y golang-go libgtk-3-dev libgtk-layer-shell0
 git clone https://github.com/nwg-piotr/nwg-bar.git
-cd nwg-bar
+cd nwg-bar || exit
 make get
 make build
 sudo make install
