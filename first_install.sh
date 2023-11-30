cc2d3fbd205d6bdc8cd3557a3f48e410425b698b
tombombadilom <tom@p113.io>
2023-11-30 11:00:19 -0300

    added mmodules/ scripts call when no package can be found

--- first_install.sh
+++ first_install.sh
@@ -1,35 +1,37 @@
 #!/usr/bin/env bash
 
-# Configuration source and target directories
+# Répertoires de configuration
 export config_dir="$HOME/.config"
 export local_dir="$HOME/.local"
-# shellcheck disable=SC2155
 export scripts="$(dirname "$0")"
 export source_dir="$scripts/.config"
 export modules_dir="$scripts/modules"
 
-# Set user locale
+# Paramètres régionaux
 export user_locale="en_US.UTF-8"
 export LANG="$user_locale"
 user_lang=${user_locale:0:2}
 export user_lang="$user_lang"
 
-# Update configuration files for Sway, Hyprland, or Wayland
+# Mise à jour des fichiers de configuration
 if command -v sway &>/dev/null; then
-  echo "export LANG=$user_locale" >>"$config_dir/sway/config"
+  echo "export LANG=$user_locale" >> "$config_dir/sway/config"
 fi
 
 if command -v hyprland &>/dev/null; then
-  echo "export LANG=$user_locale" >>"$config_dir/hyprland/hyprland.conf"
+  echo "export LANG=$user_locale" >> "$config_dir/hyprland/hyprland.conf" 
 fi
 
-# Load messages from messages.sh file
-# shellcheck disable=SC1091
+# Messages
 source ./messages.sh
 
-# Create destination directory if none exists
+# Répertoire de destination
 mkdir -p "$local_dir/bin"
-# shellcheck disable=SC2034
+
+# Liste des packages installés
+installed_packages=($(dpkg --get-selections | grep -v deinstall | cut -f1))
+
+# Liste des packages à installer
 packages=(
   "anytype"
   "betterlockscreen"
@@ -88,93 +90,34 @@ packages=(
   "wtype"
   "wunderlist"
 )
-required_packages=("make" "rsync" "git" "shellcheck" "yay" "jq") # Add any additional required packages
-missing_packages=()
 
-# Fonction pour afficher la barre de progression
-progress_bar() {
-  local duration=$1
-  # shellcheck disable=SC2155
-  local start_time=$(date +%s)
-  local end_time=$((start_time + duration))
-  local current_time=$start_time
+# Packages à installer
+to_install=()
 
-  # shellcheck disable=SC2086
-  while [ $current_time -lt $end_time ]; do
-    local current=$((current_time - start_time))
-    local total=$((end_time - start_time))
-    local progress=$((current * 100 / total))
-    local filled=$((progress / 2)) # Diviser par 2 pour ajuster à la largeur de 50
-    local unfilled=$((50 - filled))
-    echo -ne "\r\033[32m["
-    for i in $(seq 1 $filled); do echo -n '#'; done
-    # shellcheck disable=SC2034
-    for i in $(seq 1 $unfilled); do echo -n '-'; done
-    echo -ne "\033[0m] $progress%"
-    current_time=$(date +%s)
-    sleep 1
-  done
-  echo -e "\n"
-}
-
-# create log directory
-mkdir -p log
-# Install each package individually
 for package in "${packages[@]}"; do
-  echo "Installing $package..."
-  sudo apt install -y $package 2>&1 | tee "log/$package.log" || echo "Failed to install $package, skipping..."
+  if ! [[ " ${installed_packages[*]} " =~ " $package " ]]; then
+    to_install+=("$package")
+  fi
 done
 
-if [ ${#missing_packages[@]} -gt 0 ]; then
-  echo "Installing missing packages: ${missing_packages[*]}"
-  sudo apt-get install -y --no-install-recommends "${missing_packages[@]}"
-fi
+# Installation des packages
+total=${#to_install[@]}
+for ((i=1; i<=total; i++)); do
 
-# Detect operating system
-os=""
-case $(grep -oP '(?<=^ID=).+' /etc/os-release) in
-"ubuntu" | "debian" | "arch")
-  # shellcheck disable=SC2128
-  os=$BASH_REMATCH
-export os
+  package=${to_install[$i-1]}
+  
+  sudo apt-get install -y "$package"
 
-# Load packages from packages.json file
-# shellcheck disable=SC2207
-required_packages=($(jq -r '.[] | select(.isArchDependant == false) | .name' packages.json))
-missing_packages=()
-
-# Create a temporary file to store the progress
-progress_file=$(mktemp)
-
-for package in "${required_packages[@]}"; do
-  if ! dpkg -s "$package" &>/dev/null; then
-    missing_packages+=("$package")
-  fi
-  if [ -d "$modules_dir/$package.sh" ]; then
-    cd "$modules_dir/$package" || exit
-    # shellcheck disable=SC2086
-    ./$package.sh
-    cd ../
-  else
-    missing_packages+=("$package")
+  # Vérifier les modules en cas d'échec
+  if [ $? -ne 0 ]; then
+    if [ -f "$modules_dir/$package.sh" ]; then
+      "$modules_dir/$package.sh"
+    fi
   fi
 
-  # Increment the progress counter
-  ((progress_counter++))
-
-  # Calculate the progress percentage
-  # shellcheck disable=SC2154
-  progress_percentage=$((progress_counter * 100 / total_packages))
-
-  # Update progress file
-  echo "$progress_percentage" >"$progress_file"
-
+  # Barre de progression
+  printf "\rProgress : [%-50s] %d%%" $(seq -s= $i 50) $((i*100/total))
+  
 done
 
-if [ ${#missing_packages[@]} -gt 0 ]; then
-  echo "Installing missing packages: ${missing_packages[*]}"
-  sudo apt-get install -y --no-install-recommends "${missing_packages[@]}"
-fi
-
-# Remove the temporary progress file
-rm "$progress_file"
+echo "Done!"
