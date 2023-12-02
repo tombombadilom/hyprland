1c2170ba31509fef8b1a69eef0d2de8ce01f745b
tombombadilom <tom@p113.io>
2023-12-02 18:07:04 -0300

    fixed wrong install

--- modules/dunst.sh
+++ modules/dunst.sh
@@ -3,7 +3,7 @@ package="dunst"
 log_dir="log"
 log_file="$log_dir/$package.log"
 
-echo "Entering $package..." | tee "$log_file"
+echo "Installing $package..." | tee "$log_file"
 
 # Check if dependencies are already installed
 dependencies=("dbus" "libxinerama-dev" "libxrandr-dev" "libxss-dev" "libglib2.0-dev" "libpango1.0-dev" "libcairo2-dev" "libnotify-dev" "wayland-client-protocols" "xdg-utils")
@@ -14,9 +14,15 @@ for dependency in "${dependencies[@]}"; do
     fi
 done
 
-# Clone dunst repository
-git clone https://github.com/dunst-project/dunst.git
-cd dunst || exit
+# Clone dunst repository if it doesn't already exist
+if [ ! -d "dunst" ]; then
+    git clone https://github.com/dunst-project/dunst.git
+else
+    echo "dunst repository already exists. Skipping clone." | tee -a "$log_file"
+    cd dunst
+    git pull origin master
+    cd ..
+fi
 
 # Perform make operations with progress bar
 make get build 2>&1 | {
@@ -36,7 +42,6 @@ make get build 2>&1 | {
 sudo make install
 
 # Clean up
-cd ../
 rm -rf dunst
 sudo apt auto-remove -y
 
