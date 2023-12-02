5cefb4d404cce62c3b13ad060a4cf7b679cfe6aa
tombombadilom <tom@p113.io>
2023-12-02 17:35:30 -0300

    bad message need to install yad before launch

--- first_install.sh
+++ first_install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
@@ -11,9 +13,11 @@ mkdir -p "$script_dir/log"
 
 # Set configuration source and target directories
 config_dir="$HOME/.config"
+# shellcheck disable=SC2034
 local_dir="$HOME/.local"
 
 # Set source and modules directories
+# shellcheck disable=SC2034
 source_dir="$script_dir/.config"
 modules_dir="$script_dir/modules"
 
@@ -21,14 +25,13 @@ modules_dir="$script_dir/modules"
 log_file="$script_dir/log/installation.log"
 
 # Create log file if it doesn't exist
-if [ ! -f "$log_file" ]; then
-    touch "$log_file"
-fi
+touch "$log_file"
 
 # Function to set locales and install on the system
 set_locales() {
   local locale="$1"
   LANG="$locale"
+  # shellcheck disable=SC2034
   user_lang="${locale:0:2}"
 
   if ! locale -a | grep -q "$locale"; then
@@ -53,18 +56,23 @@ check_install_yad() {
   fi
 }
 
-# Prompt user to choose a language
-read -p "Choose a language (e.g., fr_FR.UTF-8): " user_locale
+# Source the packages.json file and extract the name of the array
+# shellcheck disable=SC2207
+required_packages=($(jq -r '.packages[] | .name' "$script_dir/packages.json"))
+
+# Get user's locale preference
+user_locale=""
+if [ -n "$LANG" ]; then
+  user_locale="$LANG"
+fi
+read -p "Choose a language (e.g., fr_FR.UTF-8): " -i "$user_locale" user_locale
 
 # Set locales and install on the system
 set_locales "$user_locale"
 
 # Source the messages.sh file
+# shellcheck disable=SC1091
 source "$script_dir/messages.sh"
-
-# Install required packages
-required_packages=("make" "rsync" "git" "shellcheck" "jq")
-missing_packages=()
 
 # Update package list
 echo "Updating packages..."
@@ -76,25 +84,24 @@ install_package() {
 
   if dpkg-query -W -f='${Status}' "$package" 2>/dev/null | grep -q "installed"; then
     echo "$package is already installed."
-    echo "$package" >> "$log_file"
-    return
+  else
+    echo "Installing $package..."
+    if [ -f "$modules_dir/$package.sh" ]; then
+      # shellcheck disable=SC1090
+      source "$modules_dir/$package.sh"
+    else
+      sudo apt-get install -y "$package"
+    fi
   fi
 
-  if [ -f "$modules_dir/$package.sh" ]; then
-    # shellcheck disable=SC1090
-    source "$modules_dir/$package.sh"
-    echo "$package" >> "$log_file"
-  else
-    echo "$package" >> "$log_file"
-  fi
+  echo "$package" >> "$log_file"
 }
 
 # Check and install yad if not already installed
 check_install_yad
 
 # Install packages
-# shellcheck disable=SC2154
-for package in "${packages[@]}"; do
+for package in "${required_packages[@]}"; do
   install_package "$package"
 done
 
@@ -111,12 +118,12 @@ echo "Running install_GPU.sh..."
 
 # Run checkXdgPortal.sh and log the output
 echo "Running checkXdgPortal.sh..."
-"$script_dir/checkXdgPortal.sh" >>"$log_file" 2>&1
+"$script_dir/checkXdgPortal" >> "$log_file" 2>&1
 
 # Run sync_to_system.sh and log the output
 echo "Running sync_to_system.sh..."
 "$script_dir/sync_to_system.sh" >> "$log_file" 2>&1
 
-# Reconfigure gdm3 and log the output
+# Reconfigure gdm3
 echo "Reconfiguring gdm3..."
 sudo dpkg-reconfigure gdm3
