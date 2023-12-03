4a414ab5bd9a336c3273d6b3db3d4abb7adef06b
tombombadilom <tom@p113.io>
2023-12-02 22:53:17 -0300

    update sync_to_system.sh

--- sync_to_system.sh
+++ sync_to_system.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
+
 scripts="$(dirname "$0")"
-# shellcheck disable=SC2086
 mkdir -p "$scripts/log"
+
 # Variables pour les chemins
 config_dir="$HOME/.config"
 local_dir="$HOME/.local"
 source_dir="$scripts/.config"
 local_source_dir="$scripts/.local"
-# shellcheck disable=SC2154
 log_dir="$scripts/log"
 log_file="$log_dir/sync_to_system.log"
 
-echo "Starting synchronisation to system..." | tee  "$log_file"
+echo "Starting synchronisation to system..." | tee "$log_file"
 
 # Liste des répertoires et fichiers à synchroniser
 declare -a dirs=("HybridBar" "nwg-displays" "nwg-dock" "nwg-look" "nwg-panel" "gtklock" "my_bar" "nwg-bar" "nwg-dock-hyprland" "swaync" "nwg-drawer" "sway" "paru" "volumeicon")
@@ -22,61 +22,51 @@ mkdir -p "$local_dir/bin"
 
 # Vérifie si rsync, git et shellcheck sont installés
 # Si non, installez-les
-if ! command -v rsync > /dev/null 2>&1; then
-  echo "rsync n'est pas installé. Installation en cours..." | tee -a "$log_file"
+install_package() {
+  package_name=$1
+  echo "$package_name n'est pas installé. Installation en cours..." | tee -a "$log_file"
   if [ "$(uname)" = "Darwin" ]; then
-    brew install rsync
+    brew install "$package_name"
   elif [ -x "$(command -v apt)" ]; then
-    sudo apt install -y rsync
+    sudo apt install -y "$package_name"
   else
     echo "Impossible de trouver un gestionnaire de paquets compatible." | tee -a "$log_file"
     exit 1
   fi
-fi
+}
 
-if ! command -v git > /dev/null 2>&1; then
-  echo "git n'est pas installé. Installation en cours..." | tee -a "$log_file"
-  if [ "$(uname)" = "Darwin" ]; then
-    brew install git
-  elif [ -x "$(command -v apt)" ]; then
-    sudo apt install -y git
-  else
-    echo "Impossible de trouver un gestionnaire de paquets compatible." | tee -a "$log_file"
-    exit 1
-  fi
-fi
+check_and_install_packages() {
+  packages=("$@")
+  for package in "${packages[@]}"; do
+    if ! command -v "$package" > /dev/null 2>&1; then
+      install_package "$package"
+    fi
+  done
+}
 
-if ! command -v shellcheck > /dev/null 2>&1; then
-  echo "shellcheck n'est pas installé. Installation en cours..." | tee -a "$log_file"
-  if [ -x "$(command -v apt)" ]; then
-    sudo apt install -y shellcheck
-  else 
-    echo "Impossible de trouver un gestionnaire de paquets compatible." | tee -a "$log_file"
-    exit 1
-  fi
-fi
+check_and_install_packages "rsync" "git" "shellcheck"
 
-# Synchronise les fichiers avec rsync
+# Copie les fichiers avec rsync
 rsync -av --progress --delete --exclude="${dirs[*]}" "$local_dir/bin/" "$local_source_dir/bin/" | tee -a "$log_file"
 
-# Boucle sur chaque répertoire
+# Copie le contenu des répertoires source vers les répertoires de destination
 for dir in "${dirs[@]}"; do
   if [ ! -d "$config_dir/$dir" ]; then
     echo "Création du répertoire : $config_dir/$dir" | tee -a "$log_file"
     mkdir -p "$config_dir/$dir"
   fi
 
-  # Vérifiez si le répertoire source existe avant de copier
-   if [ -d "$source_dir/$dir" ]; then
-     rsync -av --delete "$source_dir/$dir/" "$config_dir/$dir/" | tee -a "$log_file"
-   else
-     echo "Le répertoire source $source_dir/$dir n'existe pas." | tee -a "$log_file"
-   fi
- done
- 
- # Copie le fichier wayfire.ini s'il existe
- if [ -f "$source_dir/wayfire.ini" ]; then
-   cp "$source_dir/wayfire.ini" "$config_dir/" | tee -a "$log_file"
- else
-   echo "Le fichier wayfire.ini n'existe pas dans le répertoire source." | tee -a "$log_file"
- fi
+  # Vérifie si le répertoire source existe avant de copier
+  if [ -d "$source_dir/$dir" ]; then
+    cp -r "$source_dir/$dir" "$config_dir/" | tee -a "$log_file"
+  else
+    echo "Le répertoire source $source_dir/$dir n'existe pas." | tee -a "$log_file"
+  fi
+done
+
+# Copie le fichier wayfire.ini s'il existe
+if [ -f "$source_dir/wayfire.ini" ]; then
+  cp "$source_dir/wayfire.ini" "$config_dir/" | tee -a "$log_file"
+else
+  echo "Le fichier wayfire.ini n'existe pas dans le répertoire source." | tee -a "$log_file"
+fi
